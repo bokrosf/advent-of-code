09e3faf538c7df42de14947a60f2bbf5e124a009
bokrosf <bokros.feri@hotmail.com>
2024-08-30 19:12:36 +0200

    2a

---
+++ 2015/2a.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+struct box
+{
+    int length;
+    int width;
+    int height;
+
+    int surface() const;
+    int smallest_side_area() const;
+};
+
+std::istream &operator>>(std::istream &stream, box &box);
+
+int main()
+{
+    int sum = 0;
+    box box;    
+
+    while (std::cin >> box)
+    {
+        sum += box.surface() + box.smallest_side_area();
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
+
+std::istream &operator>>(std::istream &stream, box &box)
+{
+    char delimeter = 0;
+    stream >> box.length >> delimeter >> box.width >> delimeter >> box.height;
+
+    return stream; 
+}
+
+int box::surface() const
+{
+    return 2 * (width * length + width * height + height * length);
+}
+
+int box::smallest_side_area() const
+{
+    std::array<int, 3> dimensions{length, width, height};
+    std::sort(dimensions.begin(), dimensions.end());
+
+    return dimensions[0] * dimensions[1];
+}
